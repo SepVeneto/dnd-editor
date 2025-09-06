f9e83f9c56c29e263775a7177173d6f1df2d8877
SepVeneto <sepveneto@qq.com>
2025-09-06 11:24:46 +0800

    ci: init

---
+++ scripts/publish-core.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
+
+cd packages/core
+
+sed -i "s/\"version\": \"0.0.1\",/\"version\": \"$TAG_VERSION\",/" package.json
+
+pnpm build
+
+pnpm publish --no-git-checks --access public
+
+echo "✅ Publish completed"
