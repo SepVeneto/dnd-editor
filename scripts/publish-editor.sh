1959d3e6c0ce17d36db5db8f834c2e88d09a3fc9
SepVeneto <sepveneto@qq.com>
2026-01-16 16:46:27 +0800

    chore: publish

--- scripts/publish-editor.sh
+++ scripts/publish-editor.sh
@@ -12,6 +12,7 @@ sed -i "s/\"version\": \"0.0.1\",/\"version\": \"$TAG_VERSION\",/" package.json
 
 pnpm build
 
-pnpm publish --no-git-checks --access public --tag beta
+cd -
+npm publish --no-git-checks --access public -w packages/editor --tag beta
 
 echo "✅ Publish completed"
