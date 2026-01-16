144b17e2227a3ef38ecfb7c5bae08c2cda780d4d
SepVeneto <sepveneto@qq.com>
2026-01-16 16:32:54 +0800

    chore: monorepo

--- scripts/publish-plugin.sh
+++ scripts/publish-plugin.sh
@@ -10,6 +10,7 @@ sed -i "s/\"version\": \"0.0.1\",/\"version\": \"$TAG_VERSION\",/" package.json
 
 pnpm build
 
-npm publish --no-git-checks --access public
+cd -
+npm publish --no-git-checks --access public -w packages/plugins
 
 echo "✅ Publish completed"
